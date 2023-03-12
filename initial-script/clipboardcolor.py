0bf1b2ab50672d16ab24fe76ec5dcc06b99e4c9f
deatona404 <94196819+deatona404@users.noreply.github.com>
2023-03-12 17:33:56 -0400

    added function documentation

--- initial-script/clipboardcolor.py
+++ initial-script/clipboardcolor.py
@@ -1,3 +1,14 @@
+# This script converts the current clipboard text to the foreground color
+# in Krita. If the clipboard text is formatted incorrectly, it will not
+# succeed.
+#
+# This script uses regex matching to find the first valid color format
+# in the clipboard text string. 
+#
+# Supported color formats:
+#       Hexcode: AAAAAA
+#       Hexcode with #: #AAAAAA
+
 
 from PyQt5.QtGui import (
         QColor,
@@ -12,12 +23,25 @@ from krita import *
 # Some function definitions for code cleanup
 # -----------------------------------------------------------------------------
 
+'''
+    Returns a text string representing the most recent item in the system 
+    clipboard.
+
+    RETURNS: text (string)
+'''
 def getClipboardText():
     clipboard = QGuiApplication.clipboard()
     text = clipboard.text()
     return text
 
+'''
+    Returns a ManagedColor color constructed from a text string in the hexcode
+    format.
 
+    ARGUMENTS: text (string in hexcode form)
+
+    RETURNS: color (ManagedColor)
+'''
 def hexCodeToManagedColor(text) -> ManagedColor:
     color = ManagedColor("RGBA", "U8", "")
     colorComponents = color.components()
@@ -35,7 +59,13 @@ def hexCodeToManagedColor(text) -> ManagedColor:
 
     return color
 
+'''
+    Returns a ManagedColor color based on the input text string.
 
+    ARGUMENTS: text (string in a valid color format)
+
+    RETURNS: color (ManagedColor)
+'''
 def textToColor(text) -> ManagedColor:
     # TODO: there will be an if-stack here for rgb and hsl and other conversions
     # but for now it will just call hexcode
